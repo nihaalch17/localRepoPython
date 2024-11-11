d0ed4acd7b7dfb9717941a779b973b7fe3a55911
nihaalch17 <nihaalch17@gmail.com>
2024-11-11 18:30:22 +0530

    new python file added

---
+++ Test.py
@@ -0,0 +1,14 @@
+Original = "Hello World"
+S = list(Original)
+i = 0
+j = len(S)-1
+
+while(i<j):
+    temp = S[i]
+    S[i] = S[j]
+    S[j] = temp
+    i+=1
+    j-=1
+
+print("Original String: " + Original)
+print("Reversed String: " + "".join(S))
